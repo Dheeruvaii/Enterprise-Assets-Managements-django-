91086e077cfacaea0bb441e7b17173f8cbb979e9
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-31 16:55:29 +0545

    updated django-filters

--- Enterprise_Asset_project/Enterprise_Asset_project/settings.py
+++ Enterprise_Asset_project/Enterprise_Asset_project/settings.py
@@ -112,7 +112,14 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+  
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.CustomPagination",
+    "PAGE_SIZE": 5,
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
