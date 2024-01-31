70eb87c7ade5ae4681f4ef5d3e119a3b2854f240
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-31 16:38:01 +0545

    update serializer fields  methods inside meta class

--- Enterprise_Asset_project/eam_app/serializers.py
+++ Enterprise_Asset_project/eam_app/serializers.py
@@ -25,6 +25,34 @@
 # #     class Meta:
 # #         model = Assignment
 # #         fields = '__all__'
+from rest_framework import serializers
+from .models import *
+
+class AssetCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AssetCategory
+        fields = ['id', 'name', 'description']
+
+class AssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Asset
+        fields = ['id', 'asset_number', 'name', 'category', 'acquisition_date', 'purchase_cost', 'current_value', 'location', 'is_active']
+
+class MaintenanceRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MaintenanceRecord
+        fields = ['id', 'asset', 'maintenance_date', 'description', 'cost']
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ['id', 'name', 'department']
+
+# class AssignmentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Assignment
+#         fields = ['id', 'asset', 'employee', 'assignment_date', 'return_date']
+
 class AssignmentSerializer(serializers.ModelSerializer):
     # employee = EmployeeSerializer()  # Use the nested serializer for the employee field
 
