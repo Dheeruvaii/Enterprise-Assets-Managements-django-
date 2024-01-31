b4be13acdb4811c532749a217b6492e5fd088fb0
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-31 16:16:09 +0545

    add views for handle http respone

--- Enterprise_Asset_project/eam_app/views.py
+++ Enterprise_Asset_project/eam_app/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import AssetCategory, Asset, MaintenanceRecord, Employee, Assignment
+from .serializers import (
+    AssetCategorySerializer,
+    AssetSerializer,
+    MaintenanceRecordSerializer,
+    EmployeeSerializer,
+    AssignmentSerializer,
+)
+
+class AssetCategoryListCreateView(generics.ListCreateAPIView):
+    queryset = AssetCategory.objects.all()
+    serializer_class = AssetCategorySerializer
+
+class AssetListCreateView(generics.ListCreateAPIView):
+    queryset = Asset.objects.all()
+    serializer_class = AssetSerializer
+
+class MaintenanceRecordListCreateView(generics.ListCreateAPIView):
+    queryset = MaintenanceRecord.objects.all()
+    serializer_class = MaintenanceRecordSerializer
+
+class EmployeeListCreateView(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class AssignmentListCreateView(generics.ListCreateAPIView):
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentSerializer
