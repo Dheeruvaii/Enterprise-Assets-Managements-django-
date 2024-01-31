f2ed2a98af741d41edd81d118a81a42556fee846
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-31 16:37:13 +0545

    update serializer with all fields name not using fields=all methods inside meta class

--- Enterprise_Asset_project/eam_app/serializers.py
+++ Enterprise_Asset_project/eam_app/serializers.py
@@ -1,30 +1,30 @@
-from rest_framework import serializers
-from .models import AssetCategory, Asset, MaintenanceRecord, Employee, Assignment
+# from rest_framework import serializers
+# from .models import AssetCategory, Asset, MaintenanceRecord, Employee, Assignment
 
-class AssetCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AssetCategory
-        fields = '__all__'
-
-class AssetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Asset
-        fields = '__all__'
-
-class MaintenanceRecordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MaintenanceRecord
-        fields = '__all__'
-
-class EmployeeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Employee
-        fields = '__all__'
-
-# class AssignmentSerializer(serializers.ModelSerializer):
+# class AssetCategorySerializer(serializers.ModelSerializer):
 #     class Meta:
-#         model = Assignment
+#         model = AssetCategory
 #         fields = '__all__'
+
+# class AssetSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Asset
+#         fields = '__all__'
+
+# class MaintenanceRecordSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MaintenanceRecord
+#         fields = '__all__'
+
+# class EmployeeSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Employee
+#         fields = '__all__'
+
+# # class AssignmentSerializer(serializers.ModelSerializer):
+# #     class Meta:
+# #         model = Assignment
+# #         fields = '__all__'
 class AssignmentSerializer(serializers.ModelSerializer):
     # employee = EmployeeSerializer()  # Use the nested serializer for the employee field
 
