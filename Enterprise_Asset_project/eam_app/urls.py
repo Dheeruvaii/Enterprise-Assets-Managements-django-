c9811a8322d0ada693ce971ff961b4a38c5c1a8f
dheeruvaii <dhirajsapkota260@gmail.com>
2024-02-01 21:19:54 +0545

    urls change to default routers pattern

--- Enterprise_Asset_project/eam_app/urls.py
+++ Enterprise_Asset_project/eam_app/urls.py
@@ -1,16 +1,30 @@
-from django.urls import path
-from .views import (
-    AssetCategoryListCreateView,
-    AssetListCreateView,
-    MaintenanceRecordListCreateView,
-    EmployeeListCreateView,
-    AssignmentListCreateView,
-)
+# from django.urls import path
+# from .views import (
+#     AssetCategoryListCreateView,
+#     AssetListCreateView,
+#     MaintenanceRecordListCreateView,
+#     EmployeeListCreateView,
+#     AssignmentListCreateView,
+# )
+
+# urlpatterns = [
+#     path('asset-categories/', AssetCategoryListCreateView.as_view(), name='asset-category-list-create'),
+#     path('assets/', AssetListCreateView.as_view(), name='asset-list-create'),
+#     path('maintenance-records/', MaintenanceRecordListCreateView.as_view(), name='maintenance-record-list-create'),
+#     path('employees/', EmployeeListCreateView.as_view(), name='employee-list-create'),
+#     path('assignments/', AssignmentListCreateView.as_view(), name='assignment-list-create'),
+# ]
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AssetCategoryViewSet, AssetViewSet, MaintenanceRecordViewSet, EmployeeViewSet, AssignmentViewSet
+
+router = DefaultRouter()
+router.register(r'assetcategories', AssetCategoryViewSet, basename='assetcategory')
+router.register(r'assets', AssetViewSet, basename='asset')
+router.register(r'maintenancerecords', MaintenanceRecordViewSet, basename='maintenancerecord')
+router.register(r'employees', EmployeeViewSet, basename='employee')
+router.register(r'assignments', AssignmentViewSet, basename='assignment')
 
 urlpatterns = [
-    path('asset-categories/', AssetCategoryListCreateView.as_view(), name='asset-category-list-create'),
-    path('assets/', AssetListCreateView.as_view(), name='asset-list-create'),
-    path('maintenance-records/', MaintenanceRecordListCreateView.as_view(), name='maintenance-record-list-create'),
-    path('employees/', EmployeeListCreateView.as_view(), name='employee-list-create'),
-    path('assignments/', AssignmentListCreateView.as_view(), name='assignment-list-create'),
+    path('api/', include(router.urls)),
 ]
