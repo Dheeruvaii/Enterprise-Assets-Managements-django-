4f005b1306ab8d7deee7235e4db3fc96f57348ba
dheeruvaii <dhirajsapkota260@gmail.com>
2024-02-01 21:17:10 +0545

    viewsets added to all models views

--- Enterprise_Asset_project/eam_app/views.py
+++ Enterprise_Asset_project/eam_app/views.py
@@ -1,8 +1,45 @@
+# from django.shortcuts import render
+# from rest_framework import viewsets
+
+# # Create your views here.
+# from rest_framework import generics
+# from .models import AssetCategory, Asset, MaintenanceRecord, Employee, Assignment
+# from .serializers import (
+#     AssetCategorySerializer,
+#     AssetSerializer,
+#     MaintenanceRecordSerializer,
+#     EmployeeSerializer,
+#     AssignmentSerializer,
+# )
+
+# class AssetCategoryListCreateView(generics.ListCreateAPIView):
+#     queryset = AssetCategory.objects.all()
+#     serializer_class = AssetCategorySerializer
+
+# class AssetCategoryListCreateView(viewsets.ModelViewSet):
+#     queryset = AssetCategory.objects.all()
+#     serializer_class = AssetCategorySerializer
+
+# class AssetListCreateView(generics.ListCreateAPIView):
+
+
+#     queryset = Asset.objects.all()
+#     serializer_class = AssetSerializer
+#     # filterset_class=AssetFilter
+
+# class MaintenanceRecordListCreateView(generics.ListCreateAPIView):
+#     queryset = MaintenanceRecord.objects.all()
+#     serializer_class = MaintenanceRecordSerializer
+
+# class EmployeeListCreateView(generics.ListCreateAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+
+# class AssignmentListCreateView(generics.ListCreateAPIView):
+#     queryset = Assignment.objects.all()
+#     serializer_class = AssignmentSerializer
 from django.shortcuts import render
 from rest_framework import viewsets
-
-# Create your views here.
-from rest_framework import generics
 from .models import AssetCategory, Asset, MaintenanceRecord, Employee, Assignment
 from .serializers import (
     AssetCategorySerializer,
@@ -12,29 +49,23 @@ from .serializers import (
     AssignmentSerializer,
 )
 
-class AssetCategoryListCreateView(generics.ListCreateAPIView):
+class AssetCategoryViewSet(viewsets.ModelViewSet):
     queryset = AssetCategory.objects.all()
     serializer_class = AssetCategorySerializer
 
-class AssetCategoryListCreateView(viewsets.ModelViewSet):
-    queryset = AssetCategory.objects.all()
-    serializer_class = AssetCategorySerializer
-
-class AssetListCreateView(generics.ListCreateAPIView):
-
-
+class AssetViewSet(viewsets.ModelViewSet):
     queryset = Asset.objects.all()
     serializer_class = AssetSerializer
     # filterset_class=AssetFilter
 
-class MaintenanceRecordListCreateView(generics.ListCreateAPIView):
+class MaintenanceRecordViewSet(viewsets.ModelViewSet):
     queryset = MaintenanceRecord.objects.all()
     serializer_class = MaintenanceRecordSerializer
 
-class EmployeeListCreateView(generics.ListCreateAPIView):
+class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
-class AssignmentListCreateView(generics.ListCreateAPIView):
+class AssignmentViewSet(viewsets.ModelViewSet):
     queryset = Assignment.objects.all()
     serializer_class = AssignmentSerializer
